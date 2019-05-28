412a1a397576ea2399541bed55aa8fbcc16c3e21
George Kando <gkando@gmail.com>
2019-05-28 13:07:21 -0500

    first commit

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+// import moment from 'moment';
+
+const HeaderTitle = () => {
+    return (
+        <div className='header-title-wrapper'>
+            <div className='header-title'>
+                Lambda School
+                <span className='twitter-handle'> @LambdaSchool • </span>
+                <span className='time-stamp'>26 Jan</span>
+                {/* <span className='time-stamp'>{time.format('MMM Do ')}</span> */}
+            </div>   
+        </div>
+    )
+}
+
+export default HeaderTitle;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = () => {
+
+    return (
+
+        <>
+            <div className='btn white' id='clr'>clear</div>
+            <div className='btn red' id='division'>÷</div>
+            <div className='btn red' id='multiply'>×</div>
+            <div className='btn red' id='subtract'>-</div>
+            <div className='btn red' id='add'>+</div>
+            <div className='btn red' id='equals'>=</div>
+    </>
+
+
+    )
+}
+
+export default ActionButton;
